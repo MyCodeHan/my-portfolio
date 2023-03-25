c24c3b94d9835f6ba8707b1a46d38cd22d1d9c6f
hannah.harrington <hannah.harrington@accenturefederal.com>
2023-03-25 10:21:41 -0500

    feedback bug fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar() {
                     <a href="#skills" className="mr-5 hover:text-white">
                         Skills
                     </a>
-                    <a href="#testimonials" className="mr-5 hover:text-white">
+                    <a href="#feedback" className="mr-5 hover:text-white">
                         Feedback
                     </a>
                 </nav>
